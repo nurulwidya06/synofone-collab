da7b4f50bc59aafc0ee56117323562cebbfd3ab6
nurulwidya06 <nurulwidias06@gmail.com.com>
2024-06-30 14:45:59 +0700

    update landingpage dan unggulan

--- src/components/UnggulanComponent.jsx
+++ src/components/UnggulanComponent.jsx
@@ -1,120 +1,135 @@
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card} from "react-bootstrap";
 import { Link } from "react-router-dom";
+
 
 // import swiper
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 
-const UnggulanComponent = () => {
-    return (
-        <>
-            <Container>
-                <Row className="mt-5 mb-3">
-                    <Col>
-                        <h5>Smartphone Unggulan</h5>
-                    </Col>
-                </Row>
+const UnggulanComponent = ({ kategori }) => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col className="mt-5 mb-3">
+            <h5> Smartphone {kategori} </h5>
+          </Col>
+        </Row>
 
-                <Row>
-                    <Col lg={3} className="d-none d-lg-block">
-                        <Card>
-                            <Card.Img variant="top" src="../src/assets/hp1.png" />
-                            <Card.Body>
-                                <Link to="/detail" className="card-text">Xiaomi Redmi Note 13</Link>
-                                <h5>IDR 500.000</h5>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col lg={3} className="d-none d-lg-block">
-                        <Card>
-                            <Card.Img variant="top" src="../src/assets/hp2.png" />
-                            <Card.Body>
-                                <Link to="/detail" className="card-text">Xiaomi Redmi Note 13</Link>
-                                <h5>IDR 500.000</h5>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col lg={3} className="d-none d-lg-block">
-                        <Card>
-                            <Card.Img variant="top" src="../src/assets/hp3.png" />
-                            <Card.Body>
-                                <Link to="/detail" className="card-text">Xiaomi Redmi Note 13</Link>
-                                <h5>IDR 500.000</h5>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col lg={3} className="d-none d-lg-block">
-                        <Card>
-                            <Card.Img variant="top" src="../src/assets/hp4.png" />
-                            <Card.Body>
-                                <Link to="/detail" className="card-text">Xiaomi Redmi Note 13</Link>
-                                <h5>IDR 500.000</h5>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
+        <Row>
+          <Col lg={3} className="d-none d-lg-block">
+            <Card>
+              <Card.Img variant="top" src="../src/assets/hp1.png" />
+              <Card.Body>
+                <Link to="/detail" className="card-text">
+                  Xiaomi Redmi note 5
+                </Link>
+                <h5>IDR 500.000</h5>
+              </Card.Body>
+            </Card>
+          </Col>
 
-                <Row className="d-lg-none">
-                    <Swiper
-                        // install Swiper modules
-                        modules={[Pagination]}
-                        pagination={{ clickable: true }}
-                        spaceBetween={50}
-                    >
-                        <SwiperSlide>
-                            <Col lg={3}>
-                                <Card>
-                                    <Card.Img variant="top" src="../src/assets/hp1.png" />
-                                    <Card.Body>
-                                        <Link to="/detail" className="card-text">Xiaomi Redmi Note 13</Link>
-                                        <h5>IDR 500.000</h5>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </SwiperSlide>
+          <Col lg={3} className="d-none d-lg-block">
+            <Card>
+              <Card.Img variant="top" src="../src/assets/hp2.png" />
+              <Card.Body>
+                <Link to="/detail" className="card-text">
+                  Oppo A79 5G
+                </Link>
+                <h5>IDR 500.000</h5>
+              </Card.Body>
+            </Card>
+          </Col>
 
-                        <SwiperSlide>
-                            <Col lg={3}>
-                                <Card>
-                                    <Card.Img variant="top" src="../src/assets/hp2.png" />
-                                    <Card.Body>
-                                        <Link to="/detail" className="card-text">Xiaomi Redmi Note 13</Link>
-                                        <h5>IDR 500.000</h5>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </SwiperSlide>
+          <Col lg={3} className="d-none d-lg-block">
+            <Card>
+              <Card.Img variant="top" src="../src/assets/hp3.png" />
+              <Card.Body>
+                <Link to="/detail" className="card-text">
+                  iPhone 15 Pro Max
+                </Link>
+                <h5>IDR 500.000</h5>
+              </Card.Body>
+            </Card>
+          </Col>
 
-                        <SwiperSlide>
-                            <Col lg={3}>
-                                <Card>
-                                    <Card.Img variant="top" src="../src/assets/hp3.png" />
-                                    <Card.Body>
-                                        <Link to="/detail" className="card-text">Xiaomi Redmi Note 13</Link>
-                                        <h5>IDR 500.000</h5>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </SwiperSlide>
+          <Col lg={3} className="d-none d-lg-block">
+            <Card>
+              <Card.Img variant="top" src="../src/assets/hp4.png" />
+              <Card.Body>
+                <Link to="/detail" className="card-text">
+                  Samsung S23 FE
+                </Link>
+                <h5>IDR 500.000</h5>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
 
-                        <SwiperSlide>
-                            <Col lg={3}>
-                                <Card>
-                                    <Card.Img variant="top" src="../src/assets/hp4.png" />
-                                    <Card.Body>
-                                        <Link to="/detail" className="card-text">Xiaomi Redmi Note 13</Link>
-                                        <h5>IDR 500.000</h5>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </SwiperSlide>
-                    </Swiper>
-                </Row>
+        <Row className="d-lg-none">
+          <Swiper
+            modules={[Pagination]}
+            pagination={{clickable: true}}
+            spaceBetween={50}
+          >
+            <SwiperSlide>
+              <Col lg={3}>
+              <Card>
+              <Card.Img variant="top" src="../src/assets/hp1.png" />
+              <Card.Body>
+                <Link to="/detail" className="card-text">
+                  Xiaomi Redmi note 5
+                </Link>
+                <h5>IDR 500.000</h5>
+              </Card.Body>
+            </Card>
+              </Col>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Col lg={3}>
+              <Card>
+              <Card.Img variant="top" src="../src/assets/hp2.png" />
+              <Card.Body>
+                <Link to="/detail" className="card-text">
+                  Oppo A79 5G
+                </Link>
+                <h5>IDR 500.000</h5>
+              </Card.Body>
+            </Card>
+              </Col>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Col lg={3}>
+              <Card>
+              <Card.Img variant="top" src="../src/assets/hp3.png" />
+              <Card.Body>
+                <Link to="/detail" className="card-text">
+                  iPhone 15 Pro Max
+                </Link>
+                <h5>IDR 500.000</h5>
+              </Card.Body>
+              </Card> 
+              </Col>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Col lg={3}>
+              <Card>
+              <Card.Img variant="top" src="../src/assets/hp4.png" />
+              <Card.Body>
+                <Link to="/detail" className="card-text">
+                  Samsung S23 FE
+                </Link>
+                <h5>IDR 500.000</h5>
+              </Card.Body>
+            </Card>
+              </Col>
+            </SwiperSlide>
 
+          </Swiper>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
-            </Container>
-        </>
-    );
-}
-
-export default UnggulanComponent
+export default UnggulanComponent;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,33 +1,27 @@
-//import components
+// import Component
+
 import BrandComponent from "../components/BrandComponent";
 import CarouselComponent from "../components/CarouselComponent";
 import FooterComponent from "../components/FooterComponent";
-import NavComponent from "../components/NavComponent";
 import NavbarComponent from "../components/NavbarComponent";
-import SmartphoneComponent from "../components/SmartphoneComponent";
 import UnggulanComponent from "../components/UnggulanComponent";
 
 
-const LandingPage = () =>{
-    return(
+
+const LandingPage = () => {
+    return (
         <>
-            <NavbarComponent/>
+        <NavbarComponent/>
+
             <div id="banner">
                 <CarouselComponent/>
                 <BrandComponent/>
             </div>
+            
+            <UnggulanComponent kategori={"Unggulan"}/>
+            <UnggulanComponent kategori={"Lainnya"}/>
 
-            <div id ="unggulan">
-                <UnggulanComponent Category={"Unggulan"}/>
-            </div>
-
-            <div id="lainnya">
-                <UnggulanComponent Category={"Lainnya"}/>
-            </div>
-
-            <div id="footer">
-                <FooterComponent/>
-            </div>
+<FooterComponent/>
         </>
     );
 }
